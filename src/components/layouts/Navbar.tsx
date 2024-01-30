6913da58132dd42c4eb37a7a9ea75ec87dca24ca
Md Sagor Islam <sagorislam1318@gmail.com>
2024-01-30 23:14:43 +0600

    navbar responsive done

--- src/components/layouts/Navbar.tsx
+++ src/components/layouts/Navbar.tsx
@@ -1,18 +1,63 @@
 import { motion } from "framer-motion";
+import { AlignJustify } from "lucide-react";
+import { useState } from "react";
 import { NavLink } from "react-router-dom";
 
 const Navbar = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsOpen(!isOpen);
+  };
   return (
     <motion.header
       initial={{ opacity: 0 }}
       animate={{ opacity: 1, transition: { delay: 0.5, duration: 0.5 } }}
       className="w-full py-6 bg-[#F5F6F7] px-2 lg:px-0 fixed z-[999] top-0"
     >
-      <nav className=" w-full max-w-[1280px] mx-auto flex h-full justify-between items-center">
+      <nav className="w-full max-w-[1280px] mx-auto flex h-full justify-between items-center">
         <span className="text-3xl font-bold text-[#000]">
           Event<span className="text-3xl font-bold text-[#0B63E5]"> 360</span>
-        </span>{" "}
-        <ul className="space-x-8">
+        </span>
+        <div className="lg:hidden">
+          <button onClick={toggleMenu}>
+            <AlignJustify className="size-10" />
+          </button>
+          {isOpen && (
+            <ul className="absolute top-full left-0 right-0 bg-white border mx-4 shadow-md mt-1">
+              <li>
+                <NavLink
+                  to="/"
+                  className="block py-2 px-4 text-[#000] hover:bg-[#F0F5FF]"
+                  onClick={toggleMenu}
+                >
+                  Home
+                </NavLink>
+              </li>
+              <li>
+                <NavLink
+                  to="/about"
+                  className="block py-2 px-4 text-[#000] hover:bg-[#F0F5FF]"
+                  onClick={toggleMenu}
+                >
+                  About
+                </NavLink>
+              </li>
+              <li>
+                <NavLink
+                  to="/contact"
+                  className="block py-2 px-4 text-[#000] hover:bg-[#F0F5FF]"
+                  onClick={toggleMenu}
+                >
+                  Contact
+                </NavLink>
+              </li>
+            </ul>
+          )}
+        </div>
+
+        {/* For large screens: */}
+        <ul className="hidden lg:flex space-x-8">
           <NavLink to="/" className="text-base font-medium text-[#000]">
             Home
           </NavLink>
