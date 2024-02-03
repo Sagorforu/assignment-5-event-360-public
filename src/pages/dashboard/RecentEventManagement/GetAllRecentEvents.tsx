65e553ff57b7373b45ecd95734bcf3c6397aca55
Md Sagor Islam <sagorislam1318@gmail.com>
2024-02-03 22:37:53 +0600

    recent event deleted data successfully

--- src/pages/dashboard/RecentEventManagement/GetAllRecentEvents.tsx
+++ src/pages/dashboard/RecentEventManagement/GetAllRecentEvents.tsx
@@ -8,10 +8,15 @@ import {
   TableHeader,
   TableRow,
 } from "@/components/ui/table";
-import { useQuery } from "@tanstack/react-query";
+import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
 import { Pencil, Trash2 } from "lucide-react";
+import { useState } from "react";
+import UpdateModal from "./UpdateModal";
+import { ToastContainer, toast } from "react-toastify";
 
 const GetAllRecentEvents = () => {
+  const queryClient = useQueryClient();
+  const [openModal, setOpenModal] = useState(false);
   const { data, isLoading, isError } = useQuery<
     {
       _id: string;
@@ -30,6 +35,31 @@ const GetAllRecentEvents = () => {
       </div>
     );
   }
+
+  const { mutateAsync } = useMutation({
+    mutationFn: async (eventId) => {
+      const res = await fetch(
+        `http://localhost:5000/recent-events/${eventId}`,
+        {
+          method: "DELETE",
+        }
+      );
+      if (res.ok) {
+        toast("Recent Event Deleted Successfully");
+      }
+      return res.json();
+    },
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: ["recentEvents"] });
+    },
+  });
+  const handleDelete = async (eventId) => {
+    try {
+      await mutateAsync(eventId);
+    } catch (error) {
+      toast.error("Failed to Delete Recent Event.");
+    }
+  };
 
   return (
     <Container className="mt-10 lg:mt-20 border rounded">
@@ -53,20 +83,22 @@ const GetAllRecentEvents = () => {
                 </TableCell>
                 <TableCell className="font-medium">{event.name}</TableCell>
                 <TableCell>
-                  <button>
+                  <button onClick={() => setOpenModal(true)}>
                     <Pencil className="bg-[#3461FF] text-white size-8 rounded-md p-1" />
                   </button>
                 </TableCell>
                 <TableCell>
-                  <button>
+                  <button onClick={() => handleDelete(event._id)}>
                     <Trash2 className="bg-red-500 text-white size-8 rounded-md p-1" />
                   </button>
                 </TableCell>
               </TableRow>
             ))}
+            {openModal && <UpdateModal />}
           </TableBody>
         </Table>
       )}
+      <ToastContainer />
     </Container>
   );
 };
